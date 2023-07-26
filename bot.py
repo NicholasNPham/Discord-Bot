fbf56d990e4bbd0d10411aec563b9259ebd864e8
Nicholas <phamn1000@gmail.com>
2023-07-26 02:14:31 -0400

    Created a function for bot to join channel if person is in channel first.

--- bot.py
+++ bot.py
@@ -28,5 +28,13 @@ async def on_member_remove(member):
     channel = client.get_channel(1092278186886303858)
     await channel.send("Goodbye " + str(member))
 
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not in a voice channel. Join a channel first to run this command.")
+
 client.run(BOTTOKEN)
 
