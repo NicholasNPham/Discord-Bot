75fad8fae1d821170c370d10366a592aa63bc55b
Nicholas <phamn1000@gmail.com>
2023-07-26 01:35:46 -0400

    Created events that responds to members joining and leaving.

--- bot.py
+++ bot.py
@@ -4,7 +4,10 @@ from discord.ext import commands
 # API Discord Token
 from apikeys import *
 
-client = commands.Bot(command_prefix='.', intents = discord.Intents.all())       
+intents = discord.Intents.all()
+intents.members = True
+
+client = commands.Bot(command_prefix='.', intents=intents)       
 
 @client.event
 async def on_ready():
@@ -14,6 +17,16 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send('This is Bot Responding.')
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1092278186886303858)
+    await channel.send("Hello " + str(member))
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(1092278186886303858)
+    await channel.send("Goodbye " + str(member))
 
 client.run(BOTTOKEN)
 
